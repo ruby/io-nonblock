beafade64d7abb6774816cc47a8977a8bfa4c862
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-04-02 16:22:25 +0900

    bundle gem io-nonblock

---
+++ lib/io/nonblock.rb
@@ -0,0 +1,8 @@
+require "io/nonblock/version"
+
+module Io
+  module Nonblock
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/io/nonblock/version.rb
@@ -0,0 +1,5 @@
+module Io
+  module Nonblock
+    VERSION = "0.1.0"
+  end
+end
