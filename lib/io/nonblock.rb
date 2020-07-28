873e1ed87b29a0843cae8c685d1db2055e1b1dc9
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-07-28 21:02:12 +0900

    Removed needless files for test

--- lib/io/nonblock.rb
+++
@@ -1,8 +0,0 @@
-require "io/nonblock/version"
-
-module Io
-  module Nonblock
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- lib/io/nonblock/version.rb
+++
@@ -1,5 +0,0 @@
-module Io
-  module Nonblock
-    VERSION = "0.1.0"
-  end
-end
